95e4065719ba4fa3d24b86fe51a3ff2265a6ca14
NicoBeast98 <nico.aldeco@gmail.com>
2019-03-28 18:07:48 -0300

    comit local

---
+++ 58032-Nicolas-Aldeco/decimal_roman.py
@@ -0,0 +1,7 @@
+def decimal_to_roman(decimal_number):
+    roman_number = ' '
+    ant = ' '
+    for number in (decimal_number):
+        if decimal_number == 1 :
+            roman_number = 'I'
+    return(roman_number)
---
+++ 58032-Nicolas-Aldeco/test_decimal_to_roman.py
@@ -0,0 +1,3 @@
+import unittest
+from decimal_roman import decimal_to_roman
+
