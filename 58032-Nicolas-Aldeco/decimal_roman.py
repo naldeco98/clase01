8a63503f81e9c1938ead797c5b12a0caaa8edab8
NicoBeast98 <nico.aldeco@gmail.com>
2019-03-28 23:29:06 -0300

    v 0.1

--- 58032-Nicolas-Aldeco/decimal_roman.py
+++ 58032-Nicolas-Aldeco/decimal_roman.py
@@ -1,11 +1,51 @@
 def decimal_to_roman(decimal_number):
+    r_dec = ''
+    r_uni = ''
+    roman_number = ''
 
-    aux_dec = int(decimal_number/10)
-    number_x = (decimal_number/5)-(aux_dec*2)
+    num_uni = int(decimal_number/10)                
+    number_r_uni = ((decimal_number/5)-(num_uni*2))*10   #primera letra de numero romano
+    if num_uni >= 0:
+        if number_r_uni == 2:
+            r_uni = 'I'
+        if number_r_uni == 4:
+            r_uni = 'II'
+        if number_r_uni == 6:
+            r_uni = 'III'
+        if number_r_uni == 8:
+            r_uni = 'IV'
+        if number_r_uni == 10:
+            r_uni = 'V'
+        if number_r_uni == 12:
+            r_uni = 'VI'
+        if number_r_uni == 14:
+            r_uni = 'VII'
+        if number_r_uni == 16:
+            r_uni = 'VIII'
+        if number_r_uni == 18:
+            r_uni = 'IX'
 
-    if number_x == 0.2:
-        roman_number = 'I'
-    if number_x == 0.4:
-        roman_number = 'II'
-
+    if num_uni < 10 :
+        if num_uni == 1:
+            r_dec = 'X'
+        if num_uni == 2:
+            r_dec = 'XX'
+        if num_uni == 3:
+            r_dec = 'XXX'
+        if num_uni == 4:
+            r_dec = 'XL'
+        if num_uni == 5:
+            r_dec = 'L'
+        if num_uni == 6:
+            r_dec = 'LX'
+        if num_uni == 7:
+            r_dec = 'LXX'
+        if num_uni == 8:
+            r_dec = 'LXXX'
+        if num_uni == 9:
+            r_dec = 'XC'
+        if num_uni == 10:
+            r_dec = 'C'
+    roman_number = r_dec
+    roman_number += r_uni
     return (roman_number)
--- 58032-Nicolas-Aldeco/test_decimal_to_roman.py
+++ 58032-Nicolas-Aldeco/test_decimal_to_roman.py
@@ -8,12 +8,30 @@ class TestDecimalNumbers(unittest.TestCase):
     def test_decimal_2_to_roman(self):
         roman_number = decimal_to_roman (2)
         self.assertEqual(roman_number,'II')
-#    def test_decimal_3_to_roman(self):
-#        roman_number = decimal_to_roman (3)
-#        self.assertEqual(roman_number,'III')
-#    def test_decimal_4_to_roman(self):
-#        roman_number = decimal_to_roman (4)
-#        self.assertEqual(roman_number,'IV')    
+    def test_decimal_3_to_roman(self):
+        roman_number = decimal_to_roman (3)
+        self.assertEqual(roman_number,'III')
+    def test_decimal_4_to_roman(self):
+        roman_number = decimal_to_roman (4)
+        self.assertEqual(roman_number,'IV') 
+    def test_decimal_5_to_roman(self):
+        roman_number = decimal_to_roman (5)
+        self.assertEqual(roman_number,'V')   
+    def test_decimal_10_to_roman(self):
+        roman_number = decimal_to_roman (10)
+        self.assertEqual(roman_number,'X')
+    def test_decimal_12_to_roman(self):
+        roman_number = decimal_to_roman (12)
+        self.assertEqual(roman_number,'XII')   
+    def test_decimal_15_to_roman(self):
+        roman_number = decimal_to_roman (15)
+        self.assertEqual(roman_number,'XV') 
+    def test_decimal_36_to_roman(self):
+        roman_number = decimal_to_roman (36)
+        self.assertEqual(roman_number,'XXXVI')
+    def test_decimal_54_to_roman(self):
+        roman_number = decimal_to_roman (54)
+        self.assertEqual(roman_number,'LV')
 
 if __name__ == '__main__':
    unittest.main()
