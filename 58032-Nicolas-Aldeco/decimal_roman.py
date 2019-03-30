089493e98a6035ce921f5d1c7c9386ca72270b4a
NicoBeast98 <nico.aldeco@gmail.com>
2019-03-29 21:57:23 -0300

    No_sale_wey

--- 58032-Nicolas-Aldeco/decimal_roman.py
+++ 58032-Nicolas-Aldeco/decimal_roman.py
@@ -1,12 +1,30 @@
 def decimal_to_roman(decimal_number):
+
     r_dec = ''
     r_uni = ''
     r_udm = ''
     r_cent = ''
     roman_number = ''
-    num_uni = int(decimal_number/10)           
-    number_r_dec = int(round(decimal_number/10,1))      #Operacion para saber la decena
+    number_r_udm = decimal_number/1000
+    float(number_r_udm)
+    if number_r_udm == 0.999:
+        number_r_udm = 0
+    else:
+        number_r_udm = int(round(number_r_udm,2))
+
+    number_r_cent = decimal_number/100    #Operacion para saber la centena 
+    float(number_r_cent)
+    if number_r_cent == 0.99 :
+        number_r_cent = 0
+    else:
+        number_r_cent = int(round(number_r_cent,2)) 
+    
+    aux_dec = int(round(decimal_number/10,1)) * 0.1 - number_r_cent
+    number_r_dec = int(round(aux_dec,1)*10)    #Operacion para saber la decena      
+    num_uni = int(decimal_number/10)           #Auxiliar op de unidad
     number_r_uni = round((decimal_number/5)-(num_uni*2),2)   #Operacion para saber la unidad
+
+
     #Unidad#
     if number_r_uni == 0.2:
         r_uni = 'I'
@@ -47,5 +65,31 @@ def decimal_to_roman(decimal_number):
         r_dec = 'XC'
     #Centenas#
     if number_r_cent == 1: 
-    roman_number = r_udm + r_cent + r_dec + r_uni  
+        r_cent = 'C'
+    if number_r_cent == 2:
+        r_cent = 'CC'
+    if number_r_cent == 3:
+        r_cent = 'CCC'
+    if number_r_cent == 4:
+        r_cent = 'CD'
+    if number_r_cent == 5:
+        r_cent = 'D'
+    if number_r_cent == 6:
+        r_cent = 'DC'
+    if number_r_cent == 7:
+        r_cent = 'DCC'
+    if number_r_cent == 8:
+        r_cent = 'DCCC'
+    if number_r_cent == 9:
+        r_cent = 'CM'
+    #Unidades de mil#
+    if number_r_udm == 1:
+        r_udm = 'M'
+    if number_r_udm == 2:
+        r_udm = 'MM'
+    if number_r_udm == 3:
+        r_udm = 'MMM'
+     
+    roman_number = r_udm + r_cent + r_dec + r_uni
+      
     return (roman_number)
--- 58032-Nicolas-Aldeco/test_decimal_to_roman.py
+++ 58032-Nicolas-Aldeco/test_decimal_to_roman.py
@@ -32,6 +32,33 @@ class TestDecimalNumbers(unittest.TestCase):
     def test_decimal_54_to_roman(self):
         roman_number = decimal_to_roman (54)
         self.assertEqual(roman_number,'LIV')
+    def test_decimal_174_to_roman(self):
+        roman_number = decimal_to_roman (174)
+        self.assertEqual(roman_number,'CLXXIV')
+    def test_decimal_525_to_roman(self):
+        roman_number = decimal_to_roman (525)
+        self.assertEqual(roman_number,'DXXV')
+    def test_decimal_793_to_roman(self):
+        roman_number = decimal_to_roman (793)
+        self.assertEqual(roman_number,'DCCXCIII')
+    def test_decimal_999_to_roman(self):
+        roman_number = decimal_to_roman (999)
+        self.assertEqual(roman_number,'CMXCIX')
+    def test_decimal_1045_to_roman(self):
+        roman_number = decimal_to_roman (1045)
+        self.assertEqual(roman_number,'MXLV')
+    def test_decimal_1436_to_roman(self):
+        roman_number = decimal_to_roman (1436)
+        self.assertEqual(roman_number,'MCDXXXVI')
+    def test_decimal_2061_to_roman(self):
+        roman_number = decimal_to_roman (2061)
+        self.assertEqual(roman_number,'MMLXI')
+    def test_decimal_3578_to_roman(self):
+        roman_number = decimal_to_roman (3578)
+        self.assertEqual(roman_number,'MMMDLXXVIII')
+    def test_decimal_3999_to_roman(self):
+        roman_number = decimal_to_roman (3999)
+        self.assertEqual(roman_number,'MMMCMXCIX')
 
 if __name__ == '__main__':
    unittest.main()
