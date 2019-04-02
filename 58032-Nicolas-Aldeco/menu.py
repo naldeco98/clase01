e3fb749bba152016c31728dd0e4fa956a4de797f
NicoBeast98 <nico.aldeco@gmail.com>
2019-04-01 22:12:32 -0300

    v1.0

--- 58032-Nicolas-Aldeco/menu.py
+++ 58032-Nicolas-Aldeco/menu.py
@@ -16,28 +16,42 @@ while True:
     menu()
 
 
-    op_menu = input('\nIngrese un valor >>')
+    op_menu = input('\nIngrese un valor >> ')
     
     if op_menu == '1':
 
         os.system('clear')
         print('Ingrese un numero entre 1 y 3999')
         decimal_number = int(input('\n\n>> '))
+
         result = ''
-        ct = bool
-        while ct == False:
-            result = ''
-            if decimal_number < 1 and decimal_number > 4000:
-                print ('El numero esta fuera del rango')
-                ct = False
-            else:
-                result = decimal_to_roman(decimal_number)
-                ct = True
+
+        result = decimal_to_roman(decimal_number)
             
         os.system('clear')
         print(f'El numero {decimal_number} es {result} en romano')
+        input('Presione una tecla para volver...')
+        menu()
     
-    break
+    if op_menu == '2':
 
-            
-        
+        os.system('clear')
+        print('Ingrese un numero entre I y MMMCMXCIX')
+        roman_number = input('\n\n>> ').upper()
+
+        result = roman_to_decimal(roman_number)
+
+        os.system('clear')
+        if result != 0 :
+            print(f'El numero {roman_number} es {result} en decimal')
+            input('Presione una tecla para volver...')
+            menu()
+        else:
+            print('El valor ingresado es invalido')
+            input('Presione una tecla para volver...')
+            menu()
+    
+    if op_menu == '3':
+        os.system('clear')
+        print('Adios!')
+        break
