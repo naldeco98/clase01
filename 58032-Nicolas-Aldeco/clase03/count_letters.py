d2bef3a796f9383b1723376347611d41fc7fe918
NicoBeast98 <nico.aldeco@gmail.com>
2019-04-16 12:03:55 -0300

    16-Abril-19

--- 58032-Nicolas-Aldeco/clase03/count_letters.py
+++ 58032-Nicolas-Aldeco/clase03/count_letters.py
@@ -1,7 +1,8 @@
 def count_letters(frase):
     dic_mio = {}
-    let = ''
-    for let in range(frase):
-        if let in frase == dic_mio[let]:
-            dic_mio[let] =+ 1
+    for let in frase:
+        if let in dic_mio:
+            dic_mio[let] += 1
+        else:
+                dic_mio[let] = 1
     return dic_mio
